47df9ac1c6ec3f8a96b3b8a74c63308824297434
Jeffrey Cheung <jeffreycheung_@live.com>
2023-05-09 14:45:02 -0700

    wallet integration, loading, address verification

--- src/App.jsx
+++ src/App.jsx
@@ -8,19 +8,38 @@ import {
   Input,
   SimpleGrid,
   Text,
-} from '@chakra-ui/react';
-import { Alchemy, Network } from 'alchemy-sdk';
-import { useState } from 'react';
+} from "@chakra-ui/react";
+import { Alchemy, Network } from "alchemy-sdk";
+import { useState } from "react";
+import { ethers } from "ethers";
+
+const walletProvider = new ethers.providers.Web3Provider(window.ethereum);
 
 function App() {
-  const [userAddress, setUserAddress] = useState('');
+  const [userAddress, setUserAddress] = useState("");
   const [results, setResults] = useState([]);
   const [hasQueried, setHasQueried] = useState(false);
   const [tokenDataObjects, setTokenDataObjects] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  async function getAccounts() {
+    await walletProvider.send("eth_requestAccounts", []);
+
+    const signerInstance = walletProvider.getSigner();
+    const signerAddress = await signerInstance.getAddress();
+    setUserAddress(signerAddress);
+  }
 
   async function getNFTsForOwner() {
+    if (!ethers.utils.isAddress(userAddress)) {
+      alert("Please enter a valid Ethereum address.");
+      return;
+    }
+
+    setLoading(true);
+
     const config = {
-      apiKey: '<-- COPY-PASTE YOUR ALCHEMY API KEY HERE -->',
+      apiKey: import.meta.env.VITE_REACT_APP_API_KEY,
       network: Network.ETH_MAINNET,
     };
 
@@ -39,15 +58,16 @@ function App() {
     }
 
     setTokenDataObjects(await Promise.all(tokenDataPromises));
+    setLoading(false);
     setHasQueried(true);
   }
   return (
     <Box w="100vw">
       <Center>
         <Flex
-          alignItems={'center'}
+          alignItems={"center"}
           justifyContent="center"
-          flexDirection={'column'}
+          flexDirection={"column"}
         >
           <Heading mb={0} fontSize={36}>
             NFT Indexer 🖼
@@ -61,11 +81,12 @@ function App() {
         w="100%"
         flexDirection="column"
         alignItems="center"
-        justifyContent={'center'}
+        justifyContent={"center"}
       >
         <Heading mt={42}>Get all the ERC-721 tokens of this address:</Heading>
         <Input
           onChange={(e) => setUserAddress(e.target.value)}
+          value={userAddress}
           color="black"
           w="600px"
           textAlign="center"
@@ -73,6 +94,11 @@ function App() {
           bgColor="white"
           fontSize={24}
         />
+
+        <Button fontSize={20} onClick={getAccounts} mt={36} bgColor="blue">
+          Connect Account
+        </Button>
+
         <Button fontSize={20} onClick={getNFTsForOwner} mt={36} bgColor="blue">
           Fetch NFTs
         </Button>
@@ -80,36 +106,37 @@ function App() {
         <Heading my={36}>Here are your NFTs:</Heading>
 
         {hasQueried ? (
-          <SimpleGrid w={'90vw'} columns={4} spacing={24}>
+          <SimpleGrid w={"90vw"} columns={4} spacing={24}>
             {results.ownedNfts.map((e, i) => {
               return (
                 <Flex
-                  flexDir={'column'}
+                  flexDir={"column"}
                   color="white"
                   bg="blue"
-                  w={'20vw'}
+                  w={"20vw"}
                   key={e.id}
                 >
                   <Box>
-                    <b>Name:</b>{' '}
+                    <b>Name:</b>{" "}
                     {tokenDataObjects[i].title?.length === 0
-                      ? 'No Name'
+                      ? "No Name"
                       : tokenDataObjects[i].title}
                   </Box>
                   <Image
                     src={
                       tokenDataObjects[i]?.rawMetadata?.image ??
-                      'https://via.placeholder.com/200'
+                      "https://via.placeholder.com/200"
                     }
-                    alt={'Image'}
+                    alt={"Image"}
                   />
                 </Flex>
               );
             })}
           </SimpleGrid>
         ) : (
-          'Please make a query! The query may take a few seconds...'
+          "Please make a query! The query may take a few seconds..."
         )}
+        {loading && <Heading>Querying Data...</Heading>}
       </Flex>
     </Box>
   );
